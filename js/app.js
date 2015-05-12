a4d778fcbd6671e4a3620a379812b4ce34a964ab
Paulo Moggi <paulohpmoggi@gmail.com>
2015-05-12 10:42:21 -0300

    add teclado.js and animacao.js

--- js/app.js
+++ js/app.js
@@ -7,66 +7,69 @@ function DeltaTime(anterior){
 
 // ===============================================================
 
-window.onload = function()
+var canvasWidth = window.innerWidth;
+var canvasHeight = window.innerHeight;
+
+var canvas = document.createElement('canvas');
+canvas.setAttribute('width',canvasWidth);
+canvas.setAttribute('height',canvasHeight);
+document.body.appendChild(canvas);
+
+var context = canvas.getContext('2d');
+
+var teclado = new Teclado(document);
+
+startDrawing();
+
+// ===============================================================
+
+function startDrawing()
 {
-    var canvasWidth = window.innerWidth;
-    var canvasHeight = window.innerHeight;
 
-    var canvas = document.createElement('canvas');
-    canvas.setAttribute('width',canvasWidth);
-    canvas.setAttribute('height',canvasHeight);
-    document.body.appendChild(canvas);
+    var explosionSheet = new Image();
+    explosionSheet.src = 'img/explosion.png';
 
-    var context = canvas.getContext('2d');
+    var anterior = new Date().getTime();
 
-    startDrawing(context);
+    // Inicia animacao
+    setInterval(draw,1000/60);
 
-    // ===============================================================
+    // alto consumo de processamento
+    // window.requestAnimationFrame( draw );
 
-    function startDrawing(context)
+    // window.requestAnimFrame = (function(){
+    //     return  window.requestAnimationFrame    ||
+    //         window.webkitRequestAnimationFrame  ||
+    //         window.mozRequestAnimationFrame     ||
+    //         window.oRequestAnimationFrame       ||
+    //     function( callback ){
+    //         window.setTimeout(callback, 1000 / 60);
+    //       };
+    // })();
+
+    function draw()
     {
+        // limpa o canvas
+        context.clearRect(0,0,canvasWidth,canvasHeight);
 
-        var explosionSheet = new Image();
-        explosionSheet.src = 'img/explosion.png';
+        var time = anterior/50;
 
-        var anterior = new Date().getTime();
+        var xSpriteSheet = 64 * (parseInt(time)%5);
 
-        // Inicia animacao
-        setInterval(draw,1000/60);
+        var ySpriteSheet = 64 * parseInt((parseInt(time)%25)/5);
 
-        // alto consumo de processamento
-        // window.requestAnimationFrame( draw );
+        context.drawImage(explosionSheet,
+            xSpriteSheet,ySpriteSheet,64,64,  // area de recorte na imagem
+            0,0,64,64   // area no canvas (x,y,width,height)
+        );
 
-        // window.requestAnimFrame = (function(){
-        //     return  window.requestAnimationFrame    ||
-        //         window.webkitRequestAnimationFrame  ||
-        //         window.mozRequestAnimationFrame     ||
-        //         window.oRequestAnimationFrame       ||
-        //     function( callback ){
-        //         window.setTimeout(callback, 1000 / 60);
-        //       };
-        // })();
+        if( teclado.pressionada(SETA_DIREITA) )
+            console.log("direita");
 
-        function draw()
-        {
-            // limpa o canvas
-            context.clearRect(0,0,canvasWidth,canvasHeight);
+        if( teclado.pressionada(SETA_ESQUERDA) )
+            console.log("esquerda");
 
-            var time = anterior/50;
-            console.log(time);
-
-            var xSpriteSheet = 64 * (parseInt(time)%5);
-
-            var ySpriteSheet = 64 * parseInt((parseInt(time)%25)/5);
-
-            context.drawImage(explosionSheet,
-                xSpriteSheet,ySpriteSheet,64,64,  // area de recorte na imagem
-                0,0,64,64   // area no canvas (x,y,width,height)
-            );
-            anterior = new Date().getTime();
-            // requestAnimFrame(draw);
-        }
-
+        anterior = new Date().getTime();
+        // requestAnimFrame(draw);
     }
-
-};
+}
