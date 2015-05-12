671e5de549aeb73c1d6fa0ce50ec60f30fa77664
Paulo Moggi <paulohpmoggi@gmail.com>
2015-05-11 22:50:58 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,8 @@
+# HTML5 Canvas e JavaScript
+
+This README would normally document whatever steps are necessary to get your application up and running.
+
+
+## Quick Start
+
+Need nothing, just open index.html into a browser.
---
+++ js/app.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+window.onload = function()
+{
+    var canvasWidth = window.innerWidth;
+    var canvasHeight = window.innerHeight;
+
+    var canvas = document.createElement('canvas');
+    canvas.setAttribute('width',canvasWidth);
+    canvas.setAttribute('height',canvasHeight);
+    document.body.appendChild(canvas);
+
+    var context = canvas.getContext('2d');
+
+
+
+};
