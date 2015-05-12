f5a5ded54b4d3bdff2b1c29d5939a86b0de4f56d
Paulo Moggi <paulohpmoggi@gmail.com>
2015-05-11 22:52:44 -0300

    Propriedades fillStyle, strokeStyle e lineWidth

--- js/app.js
+++ js/app.js
@@ -17,5 +17,19 @@ window.onload = function()
     var context = canvas.getContext('2d');
 
 
+    startDrawing(context);
 
 };
+
+function startDrawing(context)
+{
+    // preenchimento vermelho
+    context.fillStyle = 'red';
+    context.fillRect(50,50,100,100);
+
+    // contorno azul
+    context.lineWidth =2;
+    context.strokeStyle = 'blue';
+    context.strokeRect(50,50,100,100);
+
+}
